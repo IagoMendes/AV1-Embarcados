5b7403b9ec2b235ce66e1dc67e564180fee263bf
IagoMendes <iagorm@al.insper.edu.br>
2019-04-08 15:49:57 -0300

    led e função de velocidade pronta, falta tc e rtc

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -10,6 +10,7 @@
 #include "sourcecodepro_28.h"
 #include "calibri_36.h"
 #include "arial_72.h"
+#include "stdio.h"
 
 /* Defines */
 #define YEAR        2018
@@ -20,15 +21,36 @@
 #define MINUTE      45
 #define SECOND      0
 
-#define raio 65/2
+#define raio 0.325
+volatile char string_vel[32];
+volatile char string_dist[32];
+volatile char string_time[32];
 
 // Configuracoes do botao
-#define BUT_PIO			PIOA
-#define BUT_PIO_ID		10
-#define BUT_PIO_IDX		11u
-#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX)
+// Botão1
+#define BUT1_PIO      PIOD
+#define BUT1_PIO_ID   ID_PIOD
+#define BUT1_IDX  28
+#define BUT1_IDX_MASK (1 << BUT1_IDX)
+
+// Botão2
+#define BUT2_PIO      PIOC
+#define BUT2_PIO_ID   ID_PIOC
+#define BUT2_IDX  31
+#define BUT2_IDX_MASK (1 << BUT2_IDX)
 
 struct ili9488_opt_t g_ili9488_display_opt;
+
+/* CALLBACKS */
+void but1_callback(void)
+{
+	sprintf(string_vel, "Velocidade: %dkm/h", 10);
+}
+
+void but2_callback(void)
+{
+	
+}
 
 void configure_lcd(void){
 	/* Initialize display parameter */
@@ -39,6 +61,41 @@ void configure_lcd(void){
 	/* Initialize LCD */
 	ili9488_init(&g_ili9488_display_opt);
 	ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH-1, ILI9488_LCD_HEIGHT-1);
+}
+
+void init(void){
+	board_init();
+	sysclk_init();
+	
+	// Desativa watchdog
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	// Inicializa PIO do botao
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	
+	// Configura PIO para lidar com o pino do botão como entrada
+	// com pull-up
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_IDX_MASK, PIO_PULLUP);
+	pio_configure(BUT2_PIO, PIO_INPUT, BUT2_IDX_MASK, PIO_PULLUP);
+
+	// Configura interrupção no pino referente ao botao e associa
+	// função de callback caso uma interrupção for gerada
+	// a função de callback é a: but_callback()
+	pio_handler_set(BUT1_PIO,BUT1_PIO_ID,BUT1_IDX_MASK,PIO_IT_RISE_EDGE,but1_callback);
+	pio_handler_set(BUT2_PIO,BUT2_PIO_ID,BUT2_IDX_MASK,PIO_IT_RISE_EDGE,but2_callback);
+	
+	// Ativa interrupção
+	pio_enable_interrupt(BUT1_PIO, BUT1_IDX_MASK);
+	pio_enable_interrupt(BUT2_PIO, BUT2_IDX_MASK);
+	
+	// Configura NVIC para receber interrupcoes do PIO do botao
+	// com prioridade 4 (quanto mais próximo de 0 maior)
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 4); // Prioridade 4
+	
+	NVIC_EnableIRQ(BUT2_PIO_ID);
+	NVIC_SetPriority(BUT2_PIO_ID, 4); // Prioridade 4
 }
 
 void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
@@ -56,18 +113,20 @@ void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
 }
 
 int calcula_velocidade(int n){
-	return 2*3.14*raio
+	return 2*3.14*raio*3.6;
 }
 
 int main(void) {
-	board_init();
-	sysclk_init();	
+	init();
 	configure_lcd();
 	
-	//font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
-	font_draw_text(&calibri_36, "Oi Mundo!" , 50, 100, 1);
-	//font_draw_text(&arial_72, "102456", 50, 200, 2);
+	sprintf(string_vel, "Velocidade: %d km/h", 0);
+	sprintf(string_time, "Tempo: %d", 0);
+	sprintf(string_dist, "Distância: %d", 0);
+	
 	while(1) {
-		
+		font_draw_text(&calibri_36, string_vel, 10, 100, 1);
+		font_draw_text(&calibri_36, string_dist, 10, 200, 1);
+		font_draw_text(&calibri_36, string_time, 10, 300, 1);
 	}
 }
